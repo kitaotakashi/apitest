36975c45735e33a0437274505c8664a51e65da5b
Takashi Kitao <kitao.takashi@ist.osaka-u.ac.jp>
2020-11-20 06:03:12 +0900

    add files

---
+++ front/build/precache-manifest.ef98df0aced866171d122f36d37d9406.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274b739644b35aefcf1304cb5f7f701e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49961e3aa9b25b9911eb",
+    "url": "/static/js/2.126063cd.chunk.js"
+  },
+  {
+    "revision": "728662538595f08f733af14019f4ae29",
+    "url": "/static/js/2.126063cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db90547ded163e59da2f",
+    "url": "/static/js/main.ce3b4150.chunk.js"
+  },
+  {
+    "revision": "df79543f183d7271c85a",
+    "url": "/static/js/runtime-main.333cfcd7.js"
+  }
+]);
